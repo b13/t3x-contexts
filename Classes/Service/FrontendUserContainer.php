ac0a2cd25b2d7a3a9d4920b0a01703cf4fb69677
Achim Fritz <achim.fritz@b13.de>
2020-10-19 14:41:17 +0200

    [TASK] strict types and licence info

--- Classes/Service/FrontendUserContainer.php
+++ Classes/Service/FrontendUserContainer.php
@@ -1,5 +1,16 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Netresearch\Contexts\Service;
+
+/*
+ * This file is part of TYPO3 CMS-based extension contexts by b13.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
 
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
